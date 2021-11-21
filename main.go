65ad9f1486400a154369ad1d11e79ef8afe6e6d5
Alexandre Maldémé <alma@moodagent.com>
2021-11-21 19:08:34 +0100

    Add LoggingHandler middleware

--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"todo-go/controllers"
 	"todo-go/databases"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -14,11 +16,11 @@ func main() {
 
 	h := controllers.NewBaseHandler(databases.NewInMemoryDatabase())
 
-	r.HandleFunc("/", h.RootHandler).Methods("GET")
-	r.HandleFunc("/tasks", h.GetTasks).Methods("GET")
-	r.HandleFunc("/task", h.CreateTask).Methods("POST")
-	r.HandleFunc("/task/{id:[0-9]+}", h.GetTaskByID).Methods("GET")
-	r.HandleFunc("/task/{id:[0-9]+}", h.UpdateTask).Methods("PUT")
-	r.HandleFunc("/task/{id:[0-9]+}", h.DeleteTask).Methods("DELETE")
+	r.Handle("/", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(h.RootHandler))).Methods("GET")
+	r.Handle("/tasks", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(h.GetTasks))).Methods("GET")
+	r.Handle("/task", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(h.CreateTask))).Methods("POST")
+	r.Handle("/task/{id:[0-9]+}", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(h.GetTaskByID))).Methods("GET")
+	r.Handle("/task/{id:[0-9]+}", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(h.UpdateTask))).Methods("PUT")
+	r.Handle("/task/{id:[0-9]+}", handlers.LoggingHandler(os.Stdout, http.HandlerFunc(h.DeleteTask))).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
